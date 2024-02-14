4b572c3f508d489ce059fa829c353c26a35f179b
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2024-02-14 18:37:45 -0300

    docs: added some comments

--- server/app/models/extracted_date.py
+++ server/app/models/extracted_date.py
@@ -4,6 +4,7 @@ import datetime
 class ExtractedDateSnippet:
     def __init__(self, text: str, highlight_start: int, highlight_end: int):
         self.text = text
+        # The start and end indexes of the highlighted date in the text snippet
         self.highlight_start = highlight_start
         self.highlight_end = highlight_end
 
